51f506b9359b7ea32f0384c1b6a27ecacbadb91b
gilbert98xD <100796990+gilbert98xD@users.noreply.github.com>
2022-05-01 13:02:05 +0200

    S2: evaluate.py hecho

--- code/ner/evaluate.py
+++ code/ner/evaluate.py
@@ -9,7 +9,8 @@ de la Universidad de Stanford.
 
 **Autores de los comentarios:** Gilbert Lurduy & Enrique Moreno
 
-Este módulo ...
+Este módulo contiene una función para la evaluación de la red neuronal y el main del programa
+para obtener la tasa de acierto en el conjunto de test para el modelo.
 """
 
 
@@ -23,8 +24,7 @@ import utils
 import model.net as net
 from model.data_loader import DataLoader
 
-$ pip install pycco
-$ pycco 
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', default='data/small', help="Directory containing the dataset")
@@ -36,57 +36,90 @@ parser.add_argument('--restore_file', default='best', help="name of the file in 
 
 def evaluate(model, loss_fn, data_iterator, metrics, params, num_steps):
     """
-    FUNCIONALIDADES????
+    Método que evalua el modelo con 'num_steps' batches
     """
     
-    # evaluación del modelo
+    """Evaluación del modelo"""
     model.eval()
 
     summ = []
     
     for _ in range(num_steps):
-    
-        data_batch, labels_batch = next(data_iterator) # obtenemos los datos y las etiquetas de cada batch
-        # data_iterator nos da un generador 
+        
+        """
+        Obtenemos los datos y las etiquetas de cada batch; data_iterator nos da un generador
+        """
+        data_batch, labels_batch = next(data_iterator)  
 
-        # obtención de los ouputs
+        """
+        Obtención de los ouputs
+        """
         output_batch = model(data_batch)
-        loss = loss_fn(output_batch, labels_batch) # función de périda en este batch
 
-        # se obtienen los datos outputs de las variables de antes, los copiamos en la cpu y 
-        # los convertimos en arrays 
+        """
+        Función de périda en este batch
+        """
+        loss = loss_fn(output_batch, labels_batch) 
+
+        """
+        Se obtienen los datos outputs de las variables de antes, los copiamos en la cpu y los convertimos en arrays 
+        """
         output_batch = output_batch.data.cpu().numpy()
         labels_batch = labels_batch.data.cpu().numpy()
 
-        # resumen 
+        """Resumen""" 
         summary_batch = {metric: metrics[metric](output_batch, labels_batch)
                          for metric in metrics}
         summary_batch['loss'] = loss.item()
         summ.append(summary_batch)
 
-    # media de las métricas obtenidas
+    """Media de las métricas obtenidas en la variable resumen 'summ' """
     metrics_mean = {metric:np.mean([x[metric] for x in summ]) for metric in summ[0]} 
     metrics_string = " ; ".join("{}: {:05.3f}".format(k, v) for k, v in metrics_mean.items())
     logging.info("- Eval metrics : " + metrics_string)
-    return metrics_mean
+    return metrics_mean 
 
 
 if __name__ == '__main__':
 
     args = parser.parse_args()
+    """
+    Establecimiento del camino (path)
+    """
     json_path = os.path.join(args.model_dir, 'params.json')
+
+    """
+    Con la 'keyword' assert nos asegurames de que el archivo este en el path indicado, si no 
+    saltará un error tipo AssertionError
+    """
     assert os.path.isfile(json_path), "No json configuration file found at {}".format(json_path)
+    
+    """
+    Cargamos los parámetros con ayuda de la clase Params que definimos en el archivo utils.py
+    """
     params = utils.Params(json_path)
 
-    params.cuda = torch.cuda.is_available()     # use GPU is available
+    """
+    Con este le pedimos que mejor el rendimiento mediante la GPU si es posible 
+    """
+    params.cuda = torch.cuda.is_available()     
 
-    torch.manual_seed(230)
+    torch.manual_seed(230) """ Semilla para replicar el experimento con los mismos resultados"""
     if params.cuda: torch.cuda.manual_seed(230)
         
-    utils.set_logger(os.path.join(args.model_dir, 'evaluate.log'))
+    """
+    Llamamos a la función que establece los registros
+    """
+    utils.set_logger(os.path.join(args.model_dir, 'evaluate.log')) 
 
+    """
+    Registro de nivel info que nos indica que se está creando el dataset
+    """
     logging.info("Creating the dataset...")
 
+    """
+    Carga de los datos de test
+    """
     data_loader = DataLoader(args.data_dir, params)
     data = data_loader.load_data(['test'], args.data_dir)
     test_data = data['test']
@@ -96,15 +129,31 @@ if __name__ == '__main__':
 
     logging.info("- done.")
 
+    """
+    Creamos el modelo con la clase Net
+    """
     model = net.Net(params).cuda() if params.cuda else net.Net(params)
     
+    """
+    Variables definidas de función de pérdida y métrica de tasa de acierto
+    """
     loss_fn = net.loss_fn
     metrics = net.metrics
     
+    """
+    Se indica en el registro que la evaluación comienza
+    """
     logging.info("Starting evaluation")
 
+    """
+    Cargamos los pesos del archivo con la información del modelo guardada
+    """
     utils.load_checkpoint(os.path.join(args.model_dir, args.restore_file + '.pth.tar'), model)
 
+    """
+    Evaluación: se indica el número de pasos, se usa el método de evaluación 'evaluate', guardamos
+    la tasa de acierto en el conjunto de test en un diccionario en el path 'save_path'
+    """
     num_steps = (params.test_size + 1) // params.batch_size
     test_metrics = evaluate(model, loss_fn, test_data_iterator, metrics, params, num_steps)
     save_path = os.path.join(args.model_dir, "metrics_test_{}.json".format(args.restore_file))
